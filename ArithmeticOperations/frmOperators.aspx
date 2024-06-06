cfa2112e7e4a41f0407bc8abdb2efcfd97e16c72
Brijesh Shekhda <140263791+Brijesh-103@users.noreply.github.com>
2024-06-06 22:11:43 +0530

    First Commit

---
+++ ArithmeticOperations/frmOperators.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmOperators.aspx.cs" Inherits="ArithmeticOperations.frmOperators" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblValidNo1" runat="server" Text="*" ForeColor="red" /><br />
+            <asp:Label ID="lblNo1" runat="Server" Text="Number 1" />&nbsp
+            <asp:TextBox ID="txtNumber1" runat="server" TextMode="Number" />
+            <asp:Label ID="lblNo1Error" runat="server" Text="*" ForeColor="red" /><br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:Label ID="lblValidNo2" runat="server" Text="*" ForeColor="red" /><br />
+            <asp:Label ID="lblNo2" runat="Server" Text="Number 2" />&nbsp
+            <asp:TextBox ID="txtNumber2" runat="server" TextMode="Number" /> 
+            <asp:Label ID="lblNo2Error" runat="server" Text="*" ForeColor="red" />
+        </p>
+            <p>
+                <asp:Button ID="btnADD" runat="server" OnClick="btnADD_Click" Text="ADD" />
+&nbsp;<asp:Button ID="btnSUB" runat="server" OnClick="btnSUB_Click" Text="SUB" />
+&nbsp;<asp:Button ID="btnMUL" runat="server" OnClick="btnMUL_Click" Text="MUL" />
+&nbsp;<asp:Button ID="btnDIV" runat="server" OnClick="btnDIV_Click" Text="DIV" />
+&nbsp;<asp:Button ID="btnPOW" runat="server" OnClick="btnPOW_Click" Text="POW" />
+        </p>
+           
+               
+
+            
+        <p>
+            <asp:Label ID="lblAns" runat="server"  />
+        </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ArithmeticOperations/frmOperators.aspx.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArithmeticOperations
+{
+    public partial class frmOperators : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtNumber1.Focus();
+        }
+        protected void btnADD_Click(object sender, EventArgs e)
+        {
+            lblAns.Text = string.Empty;
+            lblNo1Error.Text = "*";
+            lblNo2Error.Text = "*";
+            lblValidNo1.Text = "*";
+            lblValidNo2.Text = "*";
+           string n1 = txtNumber1.Text;
+           string n2 = txtNumber2.Text;
+            if (n1 == string.Empty & n2 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblNo2Error.Text = "Please Enter Number 2";
+            }
+            else if (n1 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblAns.Text = string.Empty;
+            }
+            else if (n2 == string.Empty)
+            {
+                lblNo2Error.Text = "Please Enter Number 2";
+                lblAns.Text = string.Empty;
+            }
+            else
+            {
+                int no1 = Convert.ToInt32(n1);
+                int no2 = Convert.ToInt32(n2);
+                if (no1 < 0 || no1 == 0 && no2 < 0 || no2 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else if (no1 < 0 || no1 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    return;
+                }
+                else if (no2 < 0 || no2 == 0)
+                {
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else
+                {
+                    int ans = no1 + no2;
+                    lblAns.Text += "Addition is : " +ans;
+                } 
+            } 
+        }
+
+        protected void btnSUB_Click(object sender, EventArgs e)
+        {
+            lblAns.Text = string.Empty;
+            lblNo1Error.Text = "*";
+            lblNo2Error.Text = "*";
+            lblValidNo1.Text = "*";
+            lblValidNo2.Text = "*";
+            string n1 = txtNumber1.Text;
+            string n2 = txtNumber2.Text;
+            if (n1 == string.Empty & n2 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblNo2Error.Text = "Please Enter Number 2";
+            }
+            else if (n1 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblAns.Text = string.Empty;
+            }
+            else if (n2 == string.Empty)
+            {
+                lblNo2Error.Text = "Please Enter Number 2";
+                lblAns.Text = string.Empty;
+            }
+            else
+            {
+                int no1 = Convert.ToInt32(n1);
+                int no2 = Convert.ToInt32(n2);
+                if (no1 < 0 || no1 == 0 && no2 < 0 || no2 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else if (no1 < 0 || no1 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";                   
+                }
+                else if (no2 < 0 || no2 == 0)
+                {
+                    lblValidNo2.Text = "Invalid Number 2.";  
+                }
+                else
+                {
+                    int ans = no1 - no2;
+                  
+                    lblAns.Text += "Subtraction is : " + Math.Abs(ans);
+                }
+            }
+        }
+
+        protected void btnMUL_Click(object sender, EventArgs e)
+        {
+            lblAns.Text = string.Empty;
+            lblNo1Error.Text = "*";
+            lblNo2Error.Text = "*";
+            lblValidNo1.Text = "*";
+            lblValidNo2.Text = "*";
+            string n1 = txtNumber1.Text;
+            string n2 = txtNumber2.Text;
+            if (n1 == string.Empty & n2 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblNo2Error.Text = "Please Enter Number 2";
+            }
+            else if (n1 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblAns.Text = string.Empty;
+            }
+            else if (n2 == string.Empty)
+            {
+                lblNo2Error.Text = "Please Enter Number 2";
+                lblAns.Text = string.Empty;
+            }
+            else
+            {
+                int no1 = Convert.ToInt32(n1);
+                int no2 = Convert.ToInt32(n2);
+                if (no1 < 0 || no1 == 0 && no2 < 0 || no2 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else if (no1 < 0 || no1 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    return;
+                }
+                else if (no2 < 0 || no2 == 0)
+                {
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else
+                {
+                    int ans = no1 * no2;
+                    lblAns.Text += "Multiaplication is : " + ans;
+                }
+            }
+        }
+
+        protected void btnDIV_Click(object sender, EventArgs e)
+        {
+            lblAns.Text = string.Empty;
+            lblNo1Error.Text = "*";
+            lblNo2Error.Text = "*";
+            lblValidNo1.Text = "*";
+            lblValidNo2.Text = "*";
+            string n1 = txtNumber1.Text;
+            string n2 = txtNumber2.Text;
+            if (n1 == string.Empty & n2 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblNo2Error.Text = "Please Enter Number 2";
+            }
+            else if (n1 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblAns.Text = string.Empty;
+            }
+            else if (n2 == string.Empty)
+            {
+                lblNo2Error.Text = "Please Enter Number 2";
+                lblAns.Text = string.Empty;
+            }
+            else
+            {
+                int no1 = Convert.ToInt32(n1);
+                int no2 = Convert.ToInt32(n2);
+                if (no1 < 0 || no1 == 0 && no2 < 0 || no2 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else if (no1 < 0 || no1 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    return;
+                }
+                else if (no2 < 0 || no2 == 0)
+                {
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else
+                {
+                    double ans = no1 / no2;
+                    lblAns.Text += "Division is : " + ans;
+                }
+            }
+        }
+
+        protected void btnPOW_Click(object sender, EventArgs e)
+        {
+            lblAns.Text = string.Empty;
+            lblNo1Error.Text = "*";
+            lblNo2Error.Text = "*";
+            lblValidNo1.Text = "*";
+            lblValidNo2.Text = "*";
+            string n1 = txtNumber1.Text;
+            string n2 = txtNumber2.Text;
+            if (n1 == string.Empty & n2 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblNo2Error.Text = "Please Enter Number 2";
+            }
+            else if (n1 == string.Empty)
+            {
+                lblNo1Error.Text = "Please Enter Number 1";
+                lblAns.Text = string.Empty;
+            }
+            else if (n2 == string.Empty)
+            {
+                lblNo2Error.Text = "Please Enter Number 2";
+                lblAns.Text = string.Empty;
+            }
+            else
+            {
+                int no1 = Convert.ToInt32(n1);
+                int no2 = Convert.ToInt32(n2);
+                if (no1 < 0 || no1 == 0 && no2 < 0 || no2 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else if (no1 < 0 || no1 == 0)
+                {
+                    lblValidNo1.Text = "Invalid Number 1.";
+                    return;
+                }
+                else if (no2 < 0 || no2 == 0)
+                {
+                    lblValidNo2.Text = "Invalid Number 2.";
+                    return;
+                }
+                else
+                {
+                    double ans = Math.Pow(no1,no2);
+                    lblAns.Text += "Power is : " + ans;
+                }
+            }
+        }
+    }
+}
+ 
